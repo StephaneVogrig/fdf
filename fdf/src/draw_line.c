85126a11e4ff4bd550d468fc9bbf80a83130c99b
Stephane Vogrig <svogrig@3C6.42angouleme.fr>
2024-02-13 09:51:25 +0100

    optimisation du zoom (suppression des calculs des lignes hors images), correction des couleurs des points.

--- fdf/src/draw_line.c
+++ fdf/src/draw_line.c
@@ -6,29 +6,29 @@
 /*   By: svogrig <svogrig@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 03:14:41 by svogrig           #+#    #+#             */
-/*   Updated: 2024/02/12 04:38:18 by svogrig          ###   ########.fr       */
+/*   Updated: 2024/02/13 09:34:33 by svogrig          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-void	draw_line_hor(t_img *img, int y, int xa, int xb)
+void	draw_line_hor(t_img *img, t_point2d a, t_point2d b)
 {
 	int	x;
 	int	end;
 
-	x = si32_min(xa, xb);
-	end = si32_max(xa, xb);
+	x = si32_min(a.x, b.x);
+	end = si32_max(a.x, b.x);
 	while (x <= end)
-		img_set_pixel(img, x++, y, 0x00FFFFFF);
+		img_set_pixel(img, x++, a.y, a.color);
 }
 
 void	draw_line_diag(t_img *img, t_point2d a, t_point2d b)
 {
-	int dx;
-	int dy;
+	int	dx;
+	int	dy;
 
-	if (a.x < b.x )
+	if (a.x < b.x)
 		dx = 1;
 	else
 		dx = -1;
@@ -40,53 +40,58 @@ void	draw_line_diag(t_img *img, t_point2d a, t_point2d b)
 	{
 		a.x += dx;
 		a.y += dy;
-		img_set_pixel(img, a.x, a.y, 0x00FF0000);
+		img_set_pixel(img, a.x, a.y, a.color);
 	}
 }
 
-void	draw_line_vert(t_img *img, int x, int ya, int yb)
+void	draw_line_vert(t_img *img, t_point2d a, t_point2d b)
 {
 	int	y;
 	int	end;
 
-	y = si32_min(ya, yb);
-	end = si32_max(ya, yb);
+	y = si32_min(a.y, b.y);
+	end = si32_max(a.y, b.y);
 	while (y <= end)
-		img_set_pixel(img, x, y++, 0x00FFFFFF);
+		img_set_pixel(img, a.x, y++, a.color);
 }
 
 void	draw_line_oblique(t_img *img, t_point2d a, t_point2d b, t_vec2i d)
 {
 	t_vec2i	abs_d;
+
 	abs_d.x = abs(d.x);
 	abs_d.y = abs(d.y);
 	if (abs_d.x == abs_d.y)
 		draw_line_diag(img, a, b);
 	else if (abs_d.y < abs_d.x)
-    {
-	 	if (a.x < b.x)
-            draw_line_bresenham_x(img, a, b, abs_d);
-     	else
-     	    draw_line_bresenham_x(img, b, a, abs_d);
-	}
-    else
 	{
-        if (a.y < b.y)
-            draw_line_bresenham_y(img, a, b, abs_d);
-        else
-            draw_line_bresenham_y(img, b, a, abs_d);
-    }
+		if (a.x < b.x)
+			draw_line_bresenham_x(img, a, b, abs_d);
+		else
+			draw_line_bresenham_x(img, b, a, abs_d);
+	}
+	else
+	{
+		if (a.y < b.y)
+			draw_line_bresenham_y(img, a, b, abs_d);
+		else
+			draw_line_bresenham_y(img, b, a, abs_d);
+	}
 }
 
 void	draw_line(t_img *img, t_point2d a, t_point2d b)
 {
 	t_vec2i	d;
 
+	if (a.x >= img->width && b.x >= img->width || a.x < 0 && b.x < 0)
+		return ;
+	if (a.y >= img->height && b.y >= img->height || a.y < 0 && b.y < 0)
+		return ;
 	// ft_printf("ax:%i, ay:%i, bx:%i, by:%i\n", a.x, a.y, b.x, b.y);
 	if (a.x == b.x)
-		draw_line_vert(img, a.x, a.y, b.y);
+		draw_line_vert(img, a, b);
 	else if (a.y == b.y)
-		draw_line_hor(img, a.y, a.x, b.x);
+		draw_line_hor(img, a, b);
 	else
 	{
 		d.x = b.x - a.x;
