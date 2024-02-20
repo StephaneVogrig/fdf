3b9c909079298d22f124965473b5f9ed090c3adc
Stephane Vogrig <svogrig@3C6.42angouleme.fr>
2024-02-20 16:20:39 +0100

    ajout : affichage width et height sur l'image

--- fdf/src/render.c
+++ fdf/src/render.c
@@ -1,14 +1,14 @@
-/******************************************************************************/
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   render.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: stephane <stephane@student.42.fr>          +#+  +:+       +#+        */
+/*   By: svogrig <svogrig@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/08 03:18:27 by svogrig           #+#    #+#             */
-/*   Updated: 2024/02/18 20:04:39 by stephane         ###   ########.fr       */
+/*   Updated: 2024/02/20 03:55:00 by svogrig          ###   ########.fr       */
 /*                                                                            */
-/******************************************************************************/
+/* ************************************************************************** */
 
 #include "fdf.h"
 
@@ -25,5 +25,15 @@ int	render(t_fdf *fdf)
 	img_clear(&fdf->map, fdf->img);
 	map_to_img(&fdf->map, fdf->img, &fdf->transform);
 	mlx_put_image_to_window(fdf->mlx, fdf->win, fdf->img, 0, 0);
+	mlx_string_put(fdf->mlx,fdf->win, 20,20,0xffffff, "Map :");
+	char	*txt;
+	txt = ft_itoa(fdf->map.nbr_col);
+	mlx_string_put(fdf->mlx,fdf->win, 30,40,0xffffff, "width :");
+	mlx_string_put(fdf->mlx,fdf->win, 100,40,0xffffff, txt);
+	free(txt);
+	txt = ft_itoa(fdf->map.nbr_line);
+	mlx_string_put(fdf->mlx,fdf->win, 30,60,0xffffff, "hight :");
+	mlx_string_put(fdf->mlx,fdf->win, 100,60,0xffffff, txt);
+	free(txt);
 	return (0);
 }
